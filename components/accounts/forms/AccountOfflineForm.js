7608dcef53d238b48efb7c7ff2b135b2e935cb59
benyamin abedian <benyamin.abedian@gmail.com>
2020-08-11 13:06:40 +0200

    User can upload an account CSV file and analyse their position

---
+++ components/accounts/forms/AccountOfflineForm.js
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react' 
+import { Row, Jumbotron } from 'react-bootstrap'
+import { AccountsContext } from '../../../pages/accounts'
+import UploadButton from '../../shared/UploadButton'
+
+export default function AccountOfflineForm() {
+    const { addAccountAndSave } = useContext(AccountsContext)
+    return <Row className='justify-content-center column' style={{ margin: '1rem'}}>
+                <Jumbotron>
+                    <p>In the offline mode you can upload a CSV file and analyse your account without saving any data</p>
+
+                    <div style={{ textAlign: 'center'}}>
+                        <UploadButton 
+                            uploadCSVFile={addAccountAndSave} 
+                            headers={['date', 'amount', 'description', 'balance', 'category']}
+                            timestamped
+                        >
+                            Upload transactions
+                        </UploadButton>
+                    </div>
+                </Jumbotron>
+            </Row>
+}
